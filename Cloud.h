8f03d4cd1c6dbd1e29dde971e2d899eb882d35c9
Nguyễn Văn Hòa <hoavannguyen1609@gmail.com>
2025-05-21 15:50:45 +0700

    Create: Cloud.cpp, Console.cpp

---
+++ Cloud.h
@@ -0,0 +1,15 @@
+#ifndef CLOUD_H
+#define CLOUD_H
+
+#include <string>
+#include <cstdlib>
+#include <sstream>
+namespace Cloud
+{
+    void backup();
+    void restore(std::string position);
+    bool is_tracked(std::string position);
+    void delete_tmp();
+}
+
+#endif // ACCOUNT_H
--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include "Console.cpp"
 
 int main()
 {
+    Console IMPLEMENT;
+	IMPLEMENT.Start_The_Program();
 }
